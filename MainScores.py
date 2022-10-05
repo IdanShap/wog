8fd9f21078a093101ba50ae4975dd78b840c6c81
IdanShap <97idan@gmail.com>
2022-10-05 18:07:41 +0300

    Added scorevoard site (not finished) MainScore.py

--- MainScores.py
+++ MainScores.py
@@ -11,15 +11,15 @@ def display_scoreboard():
         file = open(utils.SCORES_FILE_NAME, "r", encoding="utf-8")
         lines = file.readlines()
 
-        html_list = '<table>'
+        html_list = '<style>table, th, td {border:1px solid black;}</style><body><table><tr><th>User</th><th>Score</th></tr>'
         for line in lines:
             score = line.split(',')
             html_list += f'<tr><td>{score[0]}</td><td>{score[1]}</td></tr>'
-        html_list += '</table>'
+        html_list += '</table></body>'
 
         return html_list
     else:
-        return 'An error occurred, cannot find the scoreboard right now'
+        return '<h1>An error occurred, cannot find the scoreboard right now<h1>'
 
 
 app.run(host='0.0.0.0', port=80)
